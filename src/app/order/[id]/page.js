eec23b365635f09bd898aeb4755ba2f65fb361cf
kalai <kalai@cdp360.com>
2025-05-30 14:32:20 +0530

    latest code push

--- src/app/order/[id]/page.js
+++ src/app/order/[id]/page.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function page({params}) {
   const { id } = params;
-  return <div>Oder Single Id Get {id}</div>;
+  return <div>Oder Single Id Get change new {id}</div>;
 }
 
 export default page;
